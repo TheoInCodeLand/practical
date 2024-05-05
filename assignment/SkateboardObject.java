7ff8adac7a1cddad39203006ff1306acd662808f
Theophilus Thobejane <thobejanetheo@gmail.com>
2024-05-05 14:19:12 +0200

    Add Skateboard and SkateboardObject classes

---
+++ assignment/SkateboardObject.java
@@ -0,0 +1,18 @@
+public class SkateboardObject{
+
+	public static void main(String[] args){
+
+		Skateboard board = new Skateboard();
+
+		board.setBoardLength(31.5);
+
+		System.out.println(board.getBoardLength());
+
+		board.setBrand("Ally");
+
+		board.setModel("Rocket");
+
+		System.out.println(board);
+
+	}
+}
