3ca0e825c7e2701e5d0c26c4df362608ce8d8d78
Theophilus Thobejane <thobejanetheo@gmail.com>
2024-05-07 09:40:19 +0200

    "This is my first commit!"

---
+++ Documents/NetBeansProjects/Practical_two/src/main/java/com/mycompany/practical_two/Practical_two.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practical_two;
+
+/**
+ *
+ * @author user
+ */
+public class Practical_two {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("This is my other statement!");
+    }
+}
