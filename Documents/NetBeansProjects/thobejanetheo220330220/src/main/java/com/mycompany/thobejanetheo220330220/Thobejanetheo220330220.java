4eda4b84583bd271c4ec42d5204e1ac02a5503d0
Theophilus Thobejane <thobejanetheo@gmail.com>
2024-05-07 14:20:29 +0200

    "This is my first commit!"

---
+++ Documents/NetBeansProjects/thobejanetheo220330220/src/main/java/com/mycompany/thobejanetheo220330220/Thobejanetheo220330220.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.thobejanetheo220330220;
+
+/**
+ *
+ * @author user
+ */
+public class Thobejanetheo220330220 {
+
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 2;
+        int sum = x + y;
+        
+    }
+}
