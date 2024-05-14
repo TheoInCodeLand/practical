cb37605f2aeb7233e35a21177783657e74e794c8
Theophilus Thobejane <thobejanetheo@gmail.com>
2024-05-14 08:53:45 +0200

    "This is the first commit"

---
+++ Documents/NetBeansProjects/Thobejane220330220/src/main/java/com/mycompany/thobejane220330220/Thobejane220330220.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.thobejane220330220;
+
+/**
+ *
+ * @author user
+ */
+public class Thobejane220330220 {
+    public static void main(String[] args) {
+        System.out.println("Hello, Thobejane! This is your student number: 220330220");
+    }
+}
